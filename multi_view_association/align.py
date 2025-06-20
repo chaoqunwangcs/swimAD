6d6a488dd7a5a1d128485a4f89336280cd9f7d95
jianlll <wsad988888@163.com>
2025-06-20 22:40:04 +0800

    modified the multiview_tool

--- multi_view_association/align.py
+++ multi_view_association/align.py
@@ -4,13 +4,13 @@ import numpy as np
 fx, fy = 1621.9, 1856.1
 cx, cy = 1116.3, 742.9178
 
-fx_ratio = 1.4
-fy_ratio = 1.4
+fx_ratio = 1.33
+fy_ratio = 1.33
 
 fx = fx * fx_ratio
 fy = fy * fy_ratio
 
-k1, k2, k3 = -0.4893, 0.2845, -0.0906  # 径向畸变系数
+k1, k2, k3 =  -0.5353, 0.2875, -0.0906   # 径向畸变系数
 # p1, p2 = 0.00016542, 0.00076033     #切向畸变系数
 p1, p2 = 0, 0
 
@@ -21,7 +21,7 @@ K = np.array([[fx, 0, cx],
 dist_coeffs = np.array([k1, k2, p1, p2, k3])  # 径向和切向畸变系数
 
 
-image = cv2.imread("../dataset/dataset_v20250506/afternoon/3/afternoon_3_0003.jpg")
+image = cv2.imread(r"G:\project\calibration_data\morning_1_01001.jpg")
 h, w = image.shape[:2]
 
 new_K, roi = cv2.getOptimalNewCameraMatrix(
@@ -32,4 +32,4 @@ mapx, mapy = cv2.initUndistortRectifyMap(
     K, dist_coeffs, None, new_K, (w, h), cv2.CV_32FC1)
 undistorted = cv2.remap(image, mapx, mapy, cv2.INTER_LINEAR)
 
-cv2.imwrite("undistorted.jpg", undistorted)
+cv2.imwrite(r"G:\project\calibration_data\undistorted.jpg", undistorted)
